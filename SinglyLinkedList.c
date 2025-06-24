598dbe80ed783a2b6093c7e06f2c7b52bbdf4704
royalewski <zlodziejekpl@gmail.com>
2025-06-24 18:21:19 +0000

    Add files via upload

--- SinglyLinkedList.c
+++ SinglyLinkedList.c
@@ -6,10 +6,13 @@ struct Node {
   int value;
   struct Node* next;
 };
+
+
 typedef struct Node Node;
 
 void insertAtBeggining(Node **head, int input){
   Node *newNode = malloc(sizeof(Node)); 
+  newNode->next = NULL;
   if(newNode == NULL){
     printf("Allocation Failed");
     return;
@@ -21,17 +24,37 @@ void insertAtBeggining(Node **head, int input){
 void printList(Node *head){
    Node* temp = head;
   while(temp != NULL){
-    printf("\nValue: %d", temp->value);
+    printf("%d -> ", temp->value);
     temp = temp->next;
   }
   temp = head;
 }
+void insertAtEnd(Node **head, int input){
+  Node *newNode = malloc(sizeof(Node));
+  newNode->value = input;
+  if(newNode == NULL){
+    printf("Allocation Failed");
+    return;
+  }
+  Node *temp = *head;
+  if (*head == NULL) {
+    *head = newNode;
+    return;
+}
+  while(temp->next != NULL){
+    temp = temp->next;   
+  }
+  temp->next = newNode;
+}
+
 int main(){
 
   printf("\e[1;1H\e[2J");
   Node *head = NULL;
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i < 5; i++){
     insertAtBeggining(&head, i);
-  printList(head);  
+    insertAtEnd(&head, i);
+  };
+  printList(head); 
   return 0;
 }
